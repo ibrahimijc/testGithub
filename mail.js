c4f3e176686db76434d0a382fb95c1cb7f6c1f25
ibrahimijc <33429696+ibrahimijc@users.noreply.github.com>
2018-08-11 18:26:35 +0500

    auntygormint

--- mail.js
+++ mail.js
@@ -1,7 +1,7 @@
 module.exports.send = function(to, subject) {
-<<<<<<< HEAD
-  console.log('hahahahahahahakjasbd...')
-=======
-  console.log('Sending an email to shahiq Qureshi to write a proposal...')
->>>>>>> 29ede3704ecdd02955bbeb49ac0b9e621e49ad4a
+  console.log('hahahahahahahakjasbd...');
+  console.log('Sending an email to shahiq Qureshi to write a proposal...');
+  console.log('Kuch aesa Jo kbhi nei likha...');
+  console.log('mnsdammasdnmmmmmmm...')
+  console.log("yeh bik gayi hai gormint");
 }
