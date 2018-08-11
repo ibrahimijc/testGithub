f04761284a67073c102ac0e86cc0121d6610d99a
TahaAsif <k152855@nu.edu.pk>
2018-08-12 00:05:28 +0500

    asdasdasddff

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
+console.log('Attribe walay pasay dedo please dedo nah please dedo')
 
-console.log("Attribe walay pasay dedo");
-
-
-module.exports.getCustomerSync = function(id) { 
-  console.log('Hans raha hon main..');
-  return { id: id, points: 11 };
+module.exports.getCustomerSync = function(id) {
+  console.log('Hans raha hon main..')
+  return {id: id, points: 11}
 }
 
-module.exports.getCustomer = async function(id) { 
+module.exports.getCustomer = async function(id) {
   return new Promise((resolve, reject) => {
-    console.log('Reading a customer from MongoDB...');
-    resolve({ id: id, points: 11 });
-  });
+    console.log('Reading a customer from MongoDB...')
+    resolve({id: id, points: 11})
+  })
 }
