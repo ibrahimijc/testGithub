9120d6cd6e4c51fe57f1139dcfa0029bfc12a5cf
TahaAsif <k152855@nu.edu.pk>
2018-08-11 23:49:54 +0500

    asdasdasd

--- mail.js
+++ mail.js
@@ -1,3 +1,8 @@
 module.exports.send = function(to, subject) {
   console.log('Leave FYP thats for you  jgasjgdjasdasdadasd<3 <3.')
+  console.log('Leave FYP thats for you  jgasjgdjasdasdadasd<3 <3.')
+  console.log('Leave FYP thats for you  jgasjgdjasdasdadasd<3 <3.')
+  console.log('Leave FYP thats for you  jgasjgdjasdasdadasd<3 <3.')
+  console.log('Leave FYP thats for you  jgasjgdjasdasdadasd<3 <3.')
+  console.log('Leave FYP thats for you  jgasjgdjasdasdadasd<3 <3.')
 }
