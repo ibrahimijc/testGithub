339cf195a5fcab89088a0509b4f27d9f98818aab
ibrahimijc <33429696+ibrahimijc@users.noreply.github.com>
2018-08-11 17:25:33 +0500

    myCommit kuch aesa

--- mail.js
+++ mail.js
@@ -1,4 +1,5 @@
 module.exports.send = function(to, subject) {
-  console.log('hahahahahahahakjasbd...')
-  console.log('Sending an email to shahiq Qureshi to write a proposal...')
+  console.log('hahahahahahahakjasbd...');
+  console.log('Sending an email to shahiq Qureshi to write a proposal...');
+  console.log('Kuch aesa Jo kbhi nei likha...');
 }
