96b2c26b9c9396aef4930f421fca125cb3d5a260
ibrahimijc <33429696+ibrahimijc@users.noreply.github.com>
2018-08-11 22:17:24 +0500

    acceptedBothChanges

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 console.log('Attribe walay pasay dedo please dedo nah please dedo')
 
-<<<<<<< HEAD
 console.log("Attribe walay pasay mut dedo");
 
 
 module.exports.getCustomerSync = function(id) { 
   console.log('Hans raha hon main..');
   return { id: id, points: 11 };
-=======
 module.exports.getCustomerSync = function(id) {
   console.log('Hans raha hon main..')
   return {id: id, points: 11}
->>>>>>> f04761284a67073c102ac0e86cc0121d6610d99a
 }
 
 module.exports.getCustomer = async function(id) {
