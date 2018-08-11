1cfdcf160983053afd42f49939a4d0f436b48282
TahaAsif <k152855@nu.edu.pk>
2018-08-11 22:24:11 +0500

    yayayaya

--- mail.js
+++ mail.js
@@ -1,4 +1,3 @@
 module.exports.send = function(to, subject) {
-  console.log('hahahahahahahakjasbd...')
-  console.log('Sending an email to shahiq Qureshi to write a proposal...')
+  console.log('mnsdammasdnmmmmmmm...')
 }
