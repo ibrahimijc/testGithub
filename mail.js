874556a131ab603b418f4c34178528013bde66d2
TahaAsif <k152855@nu.edu.pk>
2018-08-11 23:40:39 +0500

    asdsa

--- mail.js
+++ mail.js
@@ -1,10 +1,10 @@
 module.exports.send = function(to, subject) {
-  console.log('hahahahahahahakjasbd...');
-  console.log('Sending an email to shahiq Qureshi to write a proposal...');
-  console.log('Kuch aesa Jo kbhi nei likha...');
+  console.log('hahahahahahahakjasbd...')
+  console.log('Sending an email to shahiq Qureshi to write a proposal...')
+  console.log('sdfsdfa...')
   console.log('mnsdammasdnmmmmmmm...')
-<<<<<<< HEAD
-  console.log("yeh bik gayi hai gormint");
-=======
->>>>>>> 52198b648a7413f7d97b2c25561b583970f9a222
+  console.log('mnsdammasdnmmmmmmm...')
+  console.log('mnsdammasdnmmmmmmm...')
+  console.log('mnsdammasdnmmmmmmm...')
+  console.log('mnsdammasdnmmmmmmm...')
 }
